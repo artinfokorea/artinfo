ba101f7eca99e24b9137d0e2cf90608834771891
baek <bgh9651@gmail.com>
2023-09-21 11:39:48 +0900

    Fix. build error

--- src/components/PullToRefresh.tsx
+++ src/components/PullToRefresh.tsx
@@ -1,65 +1,65 @@
-"use client"
+// "use client"
 
-import { forwardRef, ForwardedRef, useState, useEffect } from "react"
-import Loading from "./ui/Loading/Loading"
+// import { forwardRef, ForwardedRef, useState, useEffect } from "react"
+// import Loading from "./ui/Loading/Loading"
 
-type Props = {
-  refetch: () => void
-}
+// type Props = {
+//   refetch: () => void
+// }
 
-function PullToRefresh({ refetch }: Props, ref: ForwardedRef<HTMLDivElement>) {
-  const [refreshing, setRefreshing] = useState(false)
-  const [startY, setStartY] = useState(0)
-  const [test, setTest] = useState("test")
+// function PullToRefresh({ refetch }: Props, ref: ForwardedRef<HTMLDivElement>) {
+//   const [refreshing, setRefreshing] = useState(false)
+//   const [startY, setStartY] = useState(0)
+//   const [test, setTest] = useState("test")
 
-  useEffect(() => {
-    function handleTouchStart(event) {
-      setStartY(event.touches[0].clientY)
-    }
+//   useEffect(() => {
+//     function handleTouchStart(event) {
+//       setStartY(event.touches[0].clientY)
+//     }
 
-    function handleTouchMove(event) {
-      const moveY = event.touches[0].clientY
-      const pullDistance = moveY - startY
+//     function handleTouchMove(event) {
+//       const moveY = event.touches[0].clientY
+//       const pullDistance = moveY - startY
 
-      if (pullDistance > 0) {
-        event.preventDefault()
+//       if (pullDistance > 0) {
+//         event.preventDefault()
 
-        if (pullDistance > 80) {
-          ref.current.style.transform = "translate(0, 40px)"
-          ref.current.style.transition = "0.3s"
-          setRefreshing(true)
-          setTest("refresh")
-        }
-      }
-    }
+//         if (pullDistance > 80) {
+//           ref.current.style.transform = "translate(0, 40px)"
+//           ref.current.style.transition = "0.3s"
+//           setRefreshing(true)
+//           setTest("refresh")
+//         }
+//       }
+//     }
 
-    function handleTouchEnd() {
-      if (refreshing) {
-        refetch()
-        // allPostsRefetch()
-        // recentPostsRefetch()
-        setTimeout(() => {
-          setRefreshing(false)
-          ref.current.style.transform = "translate(0,0)"
-        }, 1000)
-      }
-    }
+//     function handleTouchEnd() {
+//       if (refreshing) {
+//         refetch()
+//         // allPostsRefetch()
+//         // recentPostsRefetch()
+//         setTimeout(() => {
+//           setRefreshing(false)
+//           ref.current.style.transform = "translate(0,0)"
+//         }, 1000)
+//       }
+//     }
 
-    document.addEventListener("touchstart", handleTouchStart)
-    document.addEventListener("touchmove", handleTouchMove)
-    document.addEventListener("touchend", handleTouchEnd)
+//     document.addEventListener("touchstart", handleTouchStart)
+//     document.addEventListener("touchmove", handleTouchMove)
+//     document.addEventListener("touchend", handleTouchEnd)
 
-    console.log("refreshing", refreshing)
-    console.log("test", test)
+//     console.log("refreshing", refreshing)
+//     console.log("test", test)
 
-    return () => {
-      document.removeEventListener("touchstart", handleTouchStart)
-      document.removeEventListener("touchmove", handleTouchMove)
-      document.removeEventListener("touchend", handleTouchEnd)
-    }
-  }, [refreshing, startY, ref, test])
+//     return () => {
+//       document.removeEventListener("touchstart", handleTouchStart)
+//       document.removeEventListener("touchmove", handleTouchMove)
+//       document.removeEventListener("touchend", handleTouchEnd)
+//     }
+//   }, [refreshing, startY, ref, test])
 
-  return <div>{refreshing ? <Loading /> : ""}</div>
-}
+//   return <div>{refreshing ? <Loading /> : ""}</div>
+// }
 
-export default forwardRef<HTMLDivElement, Props>(PullToRefresh)
+// export default forwardRef<HTMLDivElement, Props>(PullToRefresh)
