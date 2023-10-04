ce2b1072a64bc500f0bc7ff5e294b71a07551f62
baek <bgh9651@gmail.com>
2023-10-04 13:23:49 +0900

    Fix. bottom navigation link prefetch

--- src/components/layouts/bottom-naviation.tsx
+++ src/components/layouts/bottom-naviation.tsx
@@ -146,7 +146,7 @@ function NavItemButton({
       className={`inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group
       ${isIPhone && "py-4"}
       `}
-      prefetch={href !== "/create"}
+      prefetch={prefetch}
     >
       <Icon
         className={`${active ? activeColor : defaultColor} ${
