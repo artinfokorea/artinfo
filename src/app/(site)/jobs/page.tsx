f110fecc5b992c09a04201702bff28f1fae4000f
baek <bgh9651@gmail.com>
2023-11-06 11:01:47 +0900

    Fix. jobs, concerts layout test

--- src/app/(site)/jobs/page.tsx
+++ src/app/(site)/jobs/page.tsx
@@ -7,7 +7,7 @@ import JobContainer from "../../../components/ui/Job/JobContainer"
 
 export default function Recruits() {
   return (
-    <div className="sm:container mx-auto mt-4 px-4 h-screen">
+    <div className="sm:container mx-auto mt-4 px-4 h-screen overflow-auto">
       <div className="flex justify-between ">
         <h2 className="text-2xl font-bold mb-4">채용</h2>
         <ChipButton url="/jobs/create" title="채용등록" />
--- src/components/ui/Education/LessonCard.tsx
+++ src/components/ui/Education/LessonCard.tsx
@@ -1,15 +1,23 @@
 import LocationTag from "@/components/common/LocationTag"
 import { LESSON } from "@/types/types"
 import Image from "next/image"
-import React from "react"
+import React, { useState } from "react"
+import { Spinner } from "@material-tailwind/react"
 
 interface Props {
   lesson: LESSON
 }
 
 const LessonCard = ({ lesson }: Props) => {
+  const [isLoading, setisLoading] = useState(true)
+
   return (
     <div className="card rounded-md cursor-pointer ">
+      {isLoading && (
+        <div className="flex items-center justify-center absolute inset-0">
+          <Spinner />
+        </div>
+      )}
       <div className="overflow-hidden relative h-[240px] rounded-sm">
         <Image
           src={lesson.image_url ?? "/icon-192x192.png"}
@@ -18,6 +26,7 @@ const LessonCard = ({ lesson }: Props) => {
           fill
           quality={100}
           priority
+          onLoad={() => setisLoading(false)}
         />
       </div>
 
