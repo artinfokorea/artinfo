0927178fd4bbc4cb71459e2df0651bd01587ae77
baek <bgh9651@gmail.com>
2023-12-13 14:30:47 +0900

    Fix. job detail fixed button

--- src/components/ui/Job/JobDetailContainer.tsx
+++ src/components/ui/Job/JobDetailContainer.tsx
@@ -113,33 +113,34 @@ export default function JobDetailContainer({ jobId }: IProps) {
                   <h2 className="text-3xl font-semi-bold mb-2 break-keep">
                     {job?.title}
                   </h2>
-                  {job?.userId === user?.id ||
-                    (user?.id === "ef03de92-798d-4aa8-a750-831e97f8e889" && (
-                      <div>
-                        <button
-                          className="mr-2"
-                          onClick={() => setPageType("update")}
+
+                  {(job?.userId === user?.id ||
+                    user?.id === "ef03de92-798d-4aa8-a750-831e97f8e889") && (
+                    <div>
+                      <button
+                        className="mr-2"
+                        onClick={() => setPageType("update")}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                            />
-                          </svg>
-                        </button>
-                        <button onClick={() => setIsOpenModal(true)}>
-                          <TrashIcon className="w-9 border-r border-white pr-3" />
-                        </button>
-                      </div>
-                    ))}
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                          />
+                        </svg>
+                      </button>
+                      <button onClick={() => setIsOpenModal(true)}>
+                        <TrashIcon className="w-9 border-r border-white pr-3" />
+                      </button>
+                    </div>
+                  )}
                 </div>
                 <div className="flex items-center">
                   <div className="text-xl mr-2">{job?.companyName}</div>
