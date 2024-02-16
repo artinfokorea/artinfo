8175099a162df5b72f6917ba420118bfd24f401a
baek <bgh9651@gmail.com>
2024-02-16 15:01:29 +0900

    Fix. artist concert

--- src/components/ui/Artists/ArtistConcertCard.tsx
+++ src/components/ui/Artists/ArtistConcertCard.tsx
@@ -1,6 +1,5 @@
 import useFilters from "@/hooks/useFilters"
-import Image from "next/image"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 interface Props {
