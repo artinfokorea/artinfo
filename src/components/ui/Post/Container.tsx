0d6d1c306e8b0cb87b16daff4970d6fe146018e5
baek <bgh9651@gmail.com>
2024-03-04 11:16:55 +0900

    Fix. postContainer

--- src/components/ui/Post/Container.tsx
+++ src/components/ui/Post/Container.tsx
@@ -5,12 +5,9 @@ import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query"
 import { useInView } from "react-intersection-observer"
 import { useDidUpdate } from "@toss/react"
 import { FEED } from "@/types/types"
-import useToast from "@/hooks/useToast"
 import { getFeeds } from "@/apis/feed"
 import ListWithLatestJobs from "@/components/ui/LatestJobs/ListWithLatestJobs"
 import { useFeedMutation } from "@/hooks/useFeedMutation"
-import { useRouter } from "next/navigation"
-import LockIcon from "@/components/icons/Lock"
 import { useRef, useState } from "react"
 import { PostCard } from "./PostCard"
 import AdContainer from "../Home/ad/AdContainer"
@@ -31,9 +28,8 @@ export default function Container() {
   const queryClient = useQueryClient()
   const { user } = useAuth()
   const [secretTab, setSecretTab] = useState(false)
-  const { successToast, errorToast } = useToast()
   const containerEl = useRef<HTMLDivElement>(null)
-  const router = useRouter()
+  const { updateFeedLike, deleteFeedMutate } = useFeedMutation({ type: "post" })
 
   const [ref, inView] = useInView({
     delay: 300,
@@ -65,55 +61,6 @@ export default function Container() {
       fetchNextPage()
     }
   }, [inView, hasNextPage])
-  const { updateFeedLike, deleteFeedMutate } = useFeedMutation({ type: "post" })
-
-  // const updateFeedLikeMutation = useMutation({
-  //   mutationFn: (payload: {
-  //     like: boolean
-  //     user_id: string
-  //     post_id: number
-  //   }) => {
-  //     return updatePostLike(payload)
-  //   },
-  //   onMutate: updateLike => {
-  //     queryClient.setQueryData(["feeds", user?.id], (old: any) => {
-  //       const pages = [...old.pages]
-
-  //       pages.forEach(page => {
-  //         page.feeds.forEach((feed: FEED) => {
-  //           if (feed.feedId === updateLike.post_id) {
-  //             // eslint-disable-next-line no-param-reassign
-  //             feed.isLiking = updateLike.like
-  //             if (updateLike.like) {
-  //               // eslint-disable-next-line no-param-reassign
-  //               feed.countOfLikes += 1
-  //             } else {
-  //               // eslint-disable-next-line no-param-reassign
-  //               feed.countOfLikes -= 1
-  //             }
-  //           }
-  //         })
-  //       })
-  //       return {
-  //         pages,
-  //         pageParams: [...old.pageParams],
-  //       }
-  //     })
-  //   },
-  // })
-
-  // const deleteFeedMutation = useMutation({
-  //   mutationFn: (feedId: number) => {
-  //     return deleteFeed(feedId)
-  //   },
-  //   onError: (error: any) => {
-  //     errorToast(error.message)
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries(["feeds"])
-  //     successToast("게시글이 삭제되었습니다.")
-  //   },
-  // })
 
   const handleUpdatePostLike = (payload: {
     like: boolean
