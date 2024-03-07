a26fa5e5083bd8a8406702c18ad587977b605cad
baek <bgh9651@gmail.com>
2024-03-07 17:51:35 +0900

    Fix. sentry.client.config.ts

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,8 +1,4 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
   dsn: "https://79715ca2334425d79ff03e80d35ffaee@o4506866829885440.ingest.us.sentry.io/4506866831917056",
@@ -27,4 +23,4 @@ Sentry.init({
       blockAllMedia: true,
     }),
   ],
-});
+})
