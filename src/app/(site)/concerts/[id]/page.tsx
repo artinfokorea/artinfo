d80f3885d4356de2867220bdeb4307c3f80c38de
baek <bgh9651@gmail.com>
2024-04-23 17:58:35 +0900

    Upt. 채용 디테일 컨텐츠 필터제거

--- src/app/(site)/concerts/[id]/page.tsx
+++ src/app/(site)/concerts/[id]/page.tsx
@@ -3,12 +3,27 @@ import { Hydrate, dehydrate } from "@tanstack/react-query"
 import ConcertDetailContainer from "@/components/ui/Concert/ConcertDetailContainer"
 import { getConcert } from "@/apis/concert"
 
+// const baseURL = process.env.NEXT_PUBLIC_REST_API
+// export const revalidate = 3600
+
+// const fetchConcert = async (concertId: string) => {
+//   const res = await fetch(`${baseURL}/concerts/${concertId}`, {
+//     next: {
+//       tags: ["test"],
+//     },
+//   })
+//   return res.json()
+// }
+
 export default async function page({ params }: { params: { id: string } }) {
   const queryClient = GetQueryClient()
   await queryClient.prefetchQuery(["concert", params.id], () =>
     getConcert(params.id),
   )
   const dehydratedState = dehydrate(queryClient)
+
+  // const data = await fetchConcert(params.id)
+  // console.log("data", data.title)
 
   return (
     <Hydrate state={dehydratedState}>
